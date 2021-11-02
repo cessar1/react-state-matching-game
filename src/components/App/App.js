381bd64345625108b29e01ae0b9d47a95bbba0f0
chf05ster <chf05ster@gmail.com>
2021-11-01 21:36:46 -0300

    Handling   click events

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import OptionsPanel from "../OptionsPanel";
 import Board from "../Board";
-import { createTiles } from "../../misc/utils";
+import { createTiles, indexOfSelected } from "../../misc/utils";
 import "./App.css";
 
 class App extends Component {
@@ -15,17 +15,53 @@ class App extends Component {
       toBeCleared: null,
     };
   }
+
+  handleTileClicked(id, color) {
+    this.setState((state) => {
+      const tiles = state.tiles;
+      let toBeCleared = state.toBeCleared;
+      const selectedTileIndex = indexOfSelected(tiles, id, color);
+      const previousTileIndex = state.previousTileIndex;
+
+      if (toBeCleared !== null) {
+        tiles[toBeCleared[0]].selected = false;
+        tiles[toBeCleared[1]].selected = false;
+        toBeCleared = null;
+      }
+
+      tiles[selectedTileIndex].selected = true;
+
+      if (previousTileIndex !== null) {
+        const previousTile = tiles[previousTileIndex];
+        const selectedTile = tiles[selectedTileIndex];
+
+        if (
+          previousTile.id !== selectedTile.id &&
+          previousTile.color === color
+        ) {
+          selectedTile.matched = true;
+          previousTile.matched = true;
+          previousTileIndex = null;
+        } else {
+          toBeCleared = [previousTileIndex, selectedTileIndex];
+          previousTileIndex = null;
+        }
+      } else {
+        previousTileIndex = selectedTileIndex;
+      }
+      return { tiles, toBeCleared, previousTileIndex };
+    });
+  }
   startGame(numTiles) {
     this.setState((state) => {
       return {
         playing: true,
         previousTileIndex: null,
         toBeCleared: null,
-        tiles: createTiles(this.state.numTiles),
+        tiles: createTiles(this.state.numTiles, this.handleTileClicked),
       };
     });
   }
-
   render() {
     return (
       <div className='App'>
